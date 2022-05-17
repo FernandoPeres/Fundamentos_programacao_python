05ff3db6b74e34d76d760a11bf9f15ec581f1bff
Fernando Peres Fernandes <nando.peres13@gmail.com>
2022-05-17 14:03:52 -0400

    Update condicional.py
    Utilizando if, elif e else em um teste.

--- condicional.py
+++ condicional.py
@@ -1,6 +1,6 @@
 # CONDICIONAL
 
-numero=float(input("Digite um número: "))
+'''numero=float(input("Digite um número: "))
 
 if (numero < 10):
     print("número menor que 10")
@@ -9,5 +9,17 @@ else:
         print("Número maior que 10")
     else:
         print("Número igual a 10")
+'''
 
+numero=int(input("Digite um número: "))
 
+if numero < 0:
+    print("Número negativo!")
+elif numero == 0:
+    print("Número não negativo")
+elif numero >= 0 and numero < 10:
+    print("Número positivo menor que 10!")
+elif numero >=10 and numero <100:
+    print("Número positivo >= 10 e <100")
+else:
+    print("Número positivo >=100")
