33e0394bdb5ad76a30985c8a6813f0af64fc87e0
Fernando Peres Fernandes <nando.peres13@gmail.com>
2022-05-17 14:19:20 -0400

    Update repeticao_for.py
    Percorrendo strings com o for.

--- repeticao_for.py
+++ repeticao_for.py
@@ -27,3 +27,33 @@ tem +1 como valor da variável de incremento.
 print("Com incremento, usando valor padrão para início e incremento: ")
 for i in range(10):
     print("Aguarde!", i)
+
+#Utilizando o for para percorrer e imprimir cada letra de uma string
+nome = "fernando"
+print(nome)
+
+# inprimindo a letra de acordo com o indice atribuído
+print("for i in range(len(nome)):")
+print("     print(nome[i])")
+for i in range(len(nome)):
+    print(nome[i])
+
+# utilizando o for de outra maneira
+print("for letra in nome):")
+print("     print(letra)")
+for letra in nome:
+    print(letra)
+
+# utilizando o for para percorrer e imprimir uma lista
+lista=['a', 'e', 'i', 'o', 'u']
+print("lista=['a', 'e', 'i', 'o', 'u']")
+
+print("for i in range(len(lista)):")
+print("     print(lista[i])")
+for i in range (len(lista)):
+    print(lista[i])
+# podemos fazer referencia direta aos elementos da lista
+print("for valor in lista):")
+print("     print(lista)")
+for valor in lista:
+    print(valor)
